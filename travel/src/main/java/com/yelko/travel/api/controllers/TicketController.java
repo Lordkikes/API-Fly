4ab2b756bd36073fd1058fbd70efff31a1ae561b
Enrique Navarro <enavarro@cloudlabs.co>
2023-06-02 23:32:17 +0200

    CRUD complete to Ticket

--- travel/src/main/java/com/yelko/travel/api/controllers/TicketController.java
+++ travel/src/main/java/com/yelko/travel/api/controllers/TicketController.java
@@ -5,21 +5,37 @@ import com.yelko.travel.api.models.responses.TicketResponse;
 import com.yelko.travel.infraestructure.abstract_services.ITicketService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping(path = "/ticket")
+@RequestMapping("/ticket")
 @AllArgsConstructor
 public class TicketController {
 
     private final ITicketService ticketService;
 
-    @PostMapping
+    @GetMapping("/byId/{id}")
+    public ResponseEntity<TicketResponse> getTicket(@PathVariable UUID id){
+        return ResponseEntity.ok(ticketService.read(id));
+    }
+
+    @PostMapping("/createTicket")
     public ResponseEntity<TicketResponse> post(@RequestBody TicketRequest request){
         return ResponseEntity.ok(ticketService.create(request));
+    }
+
+    @PutMapping("/updateTicket/{id}")
+    public ResponseEntity<TicketResponse> updateTicket(@PathVariable UUID id, @RequestBody TicketRequest request){
+        return ResponseEntity.ok(this.ticketService.update(request, id));
+
+    }
+
+    @DeleteMapping("/deleteTicket/{id}")
+    public ResponseEntity<Void> deleteTicket(@PathVariable UUID id){
+        this.ticketService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- travel/src/main/java/com/yelko/travel/infraestructure/services/TicketService.java
+++ travel/src/main/java/com/yelko/travel/infraestructure/services/TicketService.java
@@ -52,21 +52,39 @@ public class TicketService implements ITicketService {
     }
 
     @Override
-    public TicketResponse read(UUID uuid) {
-        return null;
+    public TicketResponse read(UUID id) {
+
+        var ticketFromDB = this.ticketRepository.findById(id).orElseThrow();
+        return this.entityToResponse(ticketFromDB);
     }
 
     @Override
-    public TicketResponse update(TicketRequest request, UUID uuid) {
-        return null;
+    public TicketResponse update(TicketRequest request, UUID id) {
+        var ticketToUpdate = ticketRepository.findById(id).orElseThrow();
+
+        var fly = flyRepository.findById(request.getIdFly()).orElseThrow();
+
+        ticketToUpdate.setFly(fly);
+        ticketToUpdate.setPrice(BigDecimal.valueOf(0.25));
+        ticketToUpdate.setArrivalDate(LocalDateTime.now());
+        ticketToUpdate.setDepartureDate(LocalDateTime.now());
+
+        var ticketUpdated = this.ticketRepository.save(ticketToUpdate);
+
+        log.info("Ticket Updated with ID {}", ticketUpdated.getId());
+
+        return this.entityToResponse(ticketUpdated);
     }
 
     @Override
-    public void delete(UUID uuid) {
+    public void delete(UUID id) {
+
+        var ticketToDelete = ticketRepository.findById(id).orElseThrow();
+        this.ticketRepository.delete(ticketToDelete);
 
     }
 
-    private TicketResponse entityToResponse(TicketEntity entity){
+    private TicketResponse entityToResponse(TicketEntity entity) {
 
         var response = new TicketResponse();
         BeanUtils.copyProperties(entity, response);
